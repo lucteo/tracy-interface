e59ade28e5e8a45238965f56bb95edde14ee039d
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-08-10 14:36:34 +0300

    Update build scripts to use conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,11 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 import re
 from os import path
 
 class TracyInterfaceRecipe(ConanFile):
    name = "tracy-interface"
+   version = "0.1.0"
    description = "Wrapper over the Tracy, to easily include profiling abstraction into C++ applications"
    author = "Lucian Radu Teodorescu"
    topics = ("profiling", "performance", "Tracy")
@@ -12,51 +14,47 @@ class TracyInterfaceRecipe(ConanFile):
    license = "MIT"
 
    settings = "os", "compiler", "build_type", "arch"
-   generators = "cmake"
    build_policy = "missing"   # Some of the dependencies don't have builds for all our targets
 
    options = {"shared": [True, False], "fPIC": [True, False], "with_tracy_bin": [True, False]}
    default_options = {"shared": False, "fPIC": True, "with_tracy_bin": True}
 
    exports = "LICENSE"
-   exports_sources = ("src/*", "external/*", "CMakeLists.txt", "Config.cmake.in")
-
-   def set_version(self):
-      # Get the version from CMakeLists.txt project definition
-      content = tools.load(path.join(self.recipe_folder, "CMakeLists.txt"))
-      version = re.search(r"project\([^\)]+VERSION (\d+\.\d+\.\d+)[^\)]*\)", content).group(1)
-      self.version = version.strip()
+   exports_sources = ("src/*", "external/*", "CMakeLists.txt")
 
    def build_requirements(self):
       pass
 
    def config_options(self):
-       if self.settings.os == "Windows":
-           del self.options.fPIC
-           # We don't build the Tracy executable on Windows
-           del self.options.with_tracy_bin
+      if self.settings.os == "Windows":
+         self.options.rm_safe("fPIC")
+         # We don't build the Tracy executable on Windows
+         self.options.rm_safe("with_tracy_bin")
+
+   def configure(self):
+      if self.options.shared:
+         self.options.rm_safe("fPIC")
+
+   def layout(self):
+      cmake_layout(self)
+
+   def generate(self):
+      deps = CMakeDeps(self)
+      deps.generate()
+      tc = CMakeToolchain(self)
+      tc.variables["tracy-interface.with_tracy_bin"] = "ON"
+      tc.generate()
 
    def build(self):
-      # Note: options "shared" and "fPIC" are automatically handled in CMake
-      cmake = self._configure_cmake()
+      cmake = CMake(self)
+      cmake.configure()
       cmake.build()
 
    def package(self):
-      self.copy(pattern="LICENSE", dst="licenses")
-      cmake = self._configure_cmake()
+      cmake = CMake(self)
       cmake.install()
 
    def package_info(self):
-      self.cpp_info.libs = self.collect_libs()
-
-   def _configure_cmake(self):
-      cmake = CMake(self)
-      if self.settings.compiler == "Visual Studio" and self.options.shared:
-         cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
-      if self.options.with_tracy_bin:
-         cmake.definitions["tracy-interface.with_tracy_bin"] = "ON"
-
-      cmake.configure()
-      return cmake
+      self.cpp_info.libs = ["tracy-interface"]
 
 
